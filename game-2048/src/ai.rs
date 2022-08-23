225a9c090a783899c9e511676fb4035a174b7437
TobiasAichinger <aichinger.tobias13@gmail.com>
2022-08-23 19:07:02 +0200

    init ai

--- game-2048/src/ai.rs
+++ game-2048/src/ai.rs
@@ -1,9 +1,30 @@
 use bevy::prelude::*;
 
+use crate::tile::Tile;
+use crate::states::GameState;
+
 pub struct AiPlugin;
 
 impl Plugin for AiPlugin {
     fn build(&self, app: &mut App) {
-        
+        app
+            .add_system_set(
+                SystemSet::on_enter(GameState::Ai)
+                    .with_system(setup_ai)
+            )
+            .add_system_set(
+                SystemSet::on_update(GameState::Ai)
+                    .with_system(solve)
+            );
     }
+}
+
+fn setup_ai() {
+
+}
+
+fn solve(
+    mut board: Query<(Entity, &mut Transform, &mut Tile)>,
+) {
+
 }
--- game-2048/src/states.rs
+++ game-2048/src/states.rs
@@ -2,4 +2,5 @@
 pub enum GameState {
     Menu,
     Playing,
+    Ai,
 }
